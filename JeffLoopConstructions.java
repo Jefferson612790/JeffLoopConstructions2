bdd9d63f8e826298a2cca1ac8b7da936a68505aa
Jefferson612790 <jeffboy612@gmail.com>
2024-11-13 09:37:33 +0800

    Add files via upload

---
+++ JeffLoopConstructions.java
@@ -0,0 +1,94 @@
+
+package jeffloopconstructions;
+
+//import java.util.Scanner;
+
+
+public class JeffLoopConstructions {
+
+    
+   
+    public static void main(String[] args) {
+       int iNumber =3;
+        if (iNumber == 0){
+            System.out.println("first string");
+        
+        }else{
+           
+            System.out.println("second string");
+            System.out.println("third string");
+           
+            }
+    }  
+       
+               
+                
+           
+           
+          
+        
+
+
+
+
+
+          
+    
+       
+            
+        
+
+
+        
+
+           
+            
+             
+         
+         
+         
+       
+     
+     
+   
+    
+        
+
+      
+   
+           
+      
+           
+                   
+               
+           
+    
+
+
+       
+
+
+   
+
+    
+
+    
+         
+         
+         
+
+       
+    
+      
+       
+       
+       
+  
+     
+       
+     
+        }
+        
+    }
+    
+}
